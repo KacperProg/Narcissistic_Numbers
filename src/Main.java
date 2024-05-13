0cb4b00c255d73770b84fd88db897053f8f49b75
Kacper Nowak <kacper.nowak@fairtechsolutions.co.uk>
2024-05-13 16:39:04 +0100

    Initial structure

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static boolean isNarcissistic(int number) {
+        int a = 213;
+        int i = 1;
+        List<Integer> aSplit = new ArrayList<>();
+        while(i>0){
+            aSplit.add(a%10);
+
+        }
+        return false;
+    }
+
+}
